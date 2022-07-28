23dc7078ac4e04a499bb973a9c829f6cda01df06
Guillem <guillembaracco@hotmail.com>
2022-07-28 13:17:02 +0200

    iteration 3 done

--- src/App.js
+++ src/App.js
@@ -5,13 +5,25 @@ import React, {useState} from 'react';
 
 function App() {
 
-  const [contacts, useContacts ] = useState(ContactsData.slice(0, 5))
+  const showFirstFiveContacts = [...ContactsData].slice(0, 5);
+  const [contacts, setContacts] = useState(showFirstFiveContacts)
+  const lastContacts = ContactsData.slice(5);
+
+  const addRandomContact = () => {
+        let randomContact = lastContacts[Math.floor(Math.random() * lastContacts.length)];
+        const arrContacts = [...contacts]
+        arrContacts.push(randomContact)
+        setContacts(arrContacts)
+  } 
   
 
     return (
     <div className='App'>
       <h1>IronContacts</h1>
-      <table>
+      <div className='section-bt'>
+      <buttton onClick={addRandomContact} className="action-bt">Add Random Contact</buttton>
+      </div>
+      <table className='header-table'>
             <tr>
                       <th>Picture</th>
                       <th>Name</th>
@@ -20,19 +32,22 @@ function App() {
                       <th>Won an Emmy</th>
             </tr>
           </table>  
-          {contacts.map (elem => {
-              return (
-                <table key={elem.id}>
-                    <tr>
-                      <td><img src = {elem.pictureUrl} alt={elem.name} className="profilepict" /></td>
-                      <td>{elem.name}</td>
-                      <td>{parseFloat(elem.popularity).toFixed(2)}</td>
-                      <td>{elem.wonOscar ? <tl>🏆</tl> : null}</td>
-                      <td>{elem.wonEmmy ? <tl>🏆</tl> : null}</td>
-                    </tr>
-                </table>
-           )
-        })} 
+          <div className='main-table'>
+              {contacts.map (elem => {
+                  return (
+            
+                      <table>
+                          <tr>
+                            <td><img src = {elem.pictureUrl} alt={elem.name} className="profilepict" /></td>
+                            <td>{elem.name}</td>
+                            <td>{parseFloat(elem.popularity).toFixed(2)}</td>
+                            <td>{elem.wonOscar ? <tl>🏆</tl> : null}</td>
+                            <td>{elem.wonEmmy ? <tl>🏆</tl> : null}</td>
+                          </tr>
+                      </table>
+                  )
+            })} 
+         </div>
     </div>  
     )
 }
